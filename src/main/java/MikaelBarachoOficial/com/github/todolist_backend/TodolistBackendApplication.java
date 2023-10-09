5e1039ebae36b121e6676098d54d15a9e43cc5c3
Mikael <kaelbaracho@gmail.com>
2023-10-09 20:17:29 -0300

    First Commit #commitei

---
+++ src/main/java/MikaelBarachoOficial/com/github/todolist_backend/TodolistBackendApplication.java
@@ -0,0 +1,13 @@
+package MikaelBarachoOficial.com.github.todolist_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistBackendApplication.class, args);
+	}
+
+}
